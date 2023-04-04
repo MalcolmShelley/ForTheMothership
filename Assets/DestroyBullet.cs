ddaef290acffe31c19e2731fbb8f51a452e0eb8e
Dyl <117773823+dylanchristian@users.noreply.github.com>
2023-04-04 19:53:11 -0400

    main changes+ bullet destroy+shotgun sound down

--- Assets/DestroyBullet.cs
+++ Assets/DestroyBullet.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class DestroyBullet : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
+=======
+>>>>>>> Stashed changes
     void Start()
     {
         StartCoroutine(SelfDestruct());
     }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     IEnumerator SelfDestruct()
     {
         yield return new WaitForSeconds(5f);
