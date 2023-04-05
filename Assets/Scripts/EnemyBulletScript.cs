da1cbaf32f1e862ed8a318ab449cd99cd6ce3768
Dyl <117773823+dylanchristian@users.noreply.github.com>
2023-04-04 21:44:42 -0400

    newmain

--- Assets/Scripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyBulletScript.cs
@@ -23,7 +23,7 @@ public class EnemyBulletScript : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > 10)
+        if (timer > 5)
         {
             Destroy(gameObject);
         }
